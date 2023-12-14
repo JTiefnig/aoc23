fa4e2ab87b672562f23752a665402b97387f9437
Johannes Tiefnig <tiefnig@protonmail.com>
2023-12-14 17:52:35 +0100

    refactor 14

--- aoc14.py
+++ aoc14.py
@@ -1,16 +1,14 @@
-lines = []
 
+cgrid = None
 with open('input_14.txt') as f:
     lines = f.readlines()
+    cgrid = [[c for c in line.strip('\n')] for line in lines]
 
-llen = len(lines)
-print(llen)
-
-cgrid = [[c for c in line.strip('\n')] for line in lines]
 
 def printGrid(gird):
     for line in gird:
         print("".join(line))
+
 
 def movenorth(grid):
     for i, cline in enumerate(grid):
@@ -30,7 +28,7 @@ def calcLoad(grid):
         # print("rowf {}".format(llen-i))
         for c in l:
             if c == 'O':
-                load += (llen-i)
+                load += (len(grid)-i)
     return load
 
 
@@ -56,25 +54,28 @@ buffer = {}
 loopclose = 0
 looplength = 0
 
-
 for i in range(cycles):
     cgrid = doCycle(cgrid)
     id = str(cgrid)
     if id in buffer:
         loopclose = buffer[id][0] # offset of the loop
-        looplength = i - buffer[id][0] # length of the loop
+        looplength = i - buffer[id][0] #length of the loop
         break # break after first loop found
     buffer[id] = (i, calcLoad(cgrid))
 
 
-resultid = (cycles-loopclose)%looplength+loopclose-1 # -1 % start with 1 not 0
+resultid = (cycles-loopclose)%looplength+loopclose -1 
 #print(resultid)
 
 result = [b[1] for b in buffer.values()][resultid]
 print(result)
 
 # old solution: manually look for the cycle
-#for k, v in buffer.items():
-#    print("{}{} - {}".format(v[:20], len(v), loaddatabase[k]))
+# for v in [b for b in buffer.values()][(resultid-10):]:
+#     if v[0] == loopclose:
+#         print("x", end="")
+#     print("{}".format(v))
+# 
+# print("resultid: {}".format(resultid))
 # print((cycles-102)%9) - then count in the output starting with 1 where the cycle starts
 
